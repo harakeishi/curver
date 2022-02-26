07e94c5b9c01b6dce4935757e0b5716d2f20d1e3
harakeishi <hara.for.work@gmail.com>
2022-02-26 14:58:44 +0900

    説明書きの追加

--- curver.go
+++ curver.go
@@ -5,8 +5,13 @@ import (
 	"runtime/debug"
 )
 
-var Version string
+var Version string // version
 
+/*
+Returns the version as a string.
+If the Version variable is filled with a value using ldflag, it will be returned.
+Otherwise, it will return the build information embedded in the running binary.
+*/
 func GetVersion() string {
 	if Version != "" {
 		return Version
@@ -17,6 +22,9 @@ func GetVersion() string {
 	return "unknown"
 }
 
+/*
+Displays the EchoVersion value.
+*/
 func EchoVersion() {
-	fmt.Println(GetVersion())
+	fmt.Printf("version: %s", GetVersion())
 }
