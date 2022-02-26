906069990f9803e7c49aec83c8ecbd0971674597
harakeishi <hara.for.work@gmail.com>
2022-02-26 14:13:35 +0900

    test

--- curver.go
+++ curver.go
@@ -1,15 +1,22 @@
 package curver
 
 import (
+	"fmt"
 	"runtime/debug"
 )
 
-func GetVersion(version string) string {
-	if version != "" {
-		return version
+var Version string
+
+func GetVersion() string {
+	if Version != "" {
+		return Version
 	}
 	if buildInfo, ok := debug.ReadBuildInfo(); ok {
 		return buildInfo.Main.Version
 	}
 	return "unknown"
+}
+
+func EchoVersion() {
+	fmt.Println(GetVersion())
 }
